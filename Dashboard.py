18e8e7428c78a9cd4aa3eb64f4213f4cd8af7406
Rodrigomalavasi <rodrigojmalavasi@gmail.com>
2023-10-27 08:48:05 -0300

    Adicionando gráfico mensal e de barras

--- Dashboard.py
+++ Dashboard.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout='wide')
+
 def formata_numero(valor, prefixo = ''):
     for unidade in ['', 'mil']:
         if valor < 1000:
@@ -14,6 +16,13 @@ def formata_numero(valor, prefixo = ''):
 url = 'https://labdados.com/produtos'
 response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
+
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
+receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
+
+receita_categoria = dados.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço', ascending=False)
 
 with st.sidebar:
     add_input = st.text_input('Pesquisar')
@@ -41,12 +50,43 @@ fig_mapa_receita = px.scatter_geo(receita_estados,
                                     title='Receita por estado'
                                 )
 
+fig_receita_mensal =px.line(receita_mensal,
+                                x='Mes',
+                                y='Preço',
+                                markers=True,
+                                range_y=(0, receita_mensal.max()),
+                                color='Ano',
+                                line_dash='Ano',
+                                title='Receita mensal'
+                            )
+
+fig_receita_mensal.update_layout(yaxis_title='Receita')
+
+fig_receita_estados = px.bar(receita_estados.head(),
+                                x='Local da compra',
+                                y='Preço',
+                                text_auto=True,
+                                title='Top estados (receita)'
+                             )
+
+fig_receita_estados.update_layout(yaxis_title='Receita')
+
+fig_receita_categorias = px.bar(receita_categoria,
+                                    text_auto=True,
+                                    title='Receita por Categoria'
+                                )
+
+fig_receita_categorias.update_layout(yaxis_title='Receita')
+
 ## Visualização do Streamlit
 coluna1, coluna2 = st.columns(2)
 with coluna1: 
     st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
-    st.plotly_chart(fig_mapa_receita)
+    st.plotly_chart(fig_mapa_receita, use_container_width=True)
+    st.plotly_chart(fig_receita_estados, use_container_width=True)
 with coluna2:
     st.metric('Quantidade de vendas', formata_numero(dados.shape[0]))
+    st.plotly_chart(fig_receita_mensal, use_container_width=True)
+    st.plotly_chart(fig_receita_categorias, use_container_width=True)
 
 st.dataframe(dados)
