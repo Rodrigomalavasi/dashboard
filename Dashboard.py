253cbceeccb1e46c774e8c53beeff572d149959c
Rodrigomalavasi <rodrigojmalavasi@gmail.com>
2023-10-27 08:05:10 -0300

    Novo mapa sobre quantidade e local de vendas em cada estado em Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -25,9 +25,27 @@ with st.sidebar:
     
 st.title('DASHBOARD DE VENDAS :shopping_trolley:')
 
+## Tabelas
+receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
+receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
+
+## Gráficos 
+fig_mapa_receita = px.scatter_geo(receita_estados, 
+                                    lat='lat', 
+                                    lon='lon', 
+                                    scope='south america', 
+                                    size='Preço',
+                                    template='seaborn',
+                                    hover_name='Local da compra',
+                                    hover_data= {'lat':False, 'lon':False},
+                                    title='Receita por estado'
+                                )
+
+## Visualização do Streamlit
 coluna1, coluna2 = st.columns(2)
 with coluna1: 
     st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+    st.plotly_chart(fig_mapa_receita)
 with coluna2:
     st.metric('Quantidade de vendas', formata_numero(dados.shape[0]))
 
